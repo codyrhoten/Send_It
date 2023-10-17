2507b61870418a57d63ae00ccb0a27ab5ea76d69
codyrhoten <rhoten_cody@yahoo.com>
2023-10-17 06:53:17 -0400

    Update workspace compiler version & add getCreatedOrders() to Orders contract

--- sendit.smart-contracts/contracts/Orders.sol
+++ sendit.smart-contracts/contracts/Orders.sol
@@ -16,24 +16,25 @@ contract Orders {
         uint256 amount;
         string fromCurrency;
         address toCurrency;
+        string locationId;
         OrderStatus status;
     }
 
     Order[] public orders;
-    mapping(address => uint[]) public senderToOrders;
-    mapping(address => uint[]) public courierToOrders;
+    mapping(address => uint256[]) public senderToOrders;
+    mapping(address => uint256[]) public courierToOrders;
 
     event OrderCreated(
-        uint indexed orderId,
+        uint256 indexed orderId,
         address sender,
         address receiver,
         uint256 amount,
         string fromCurrency,
         address toCurrency
     );
-    event OrderInProgress(uint indexed orderId, address courier);
+    event OrderInProgress(uint256 indexed orderId, address courier);
     event OrderCompleted(
-        uint indexed orderId,
+        uint256 indexed orderId,
         address sender,
         address receiver,
         uint256 amount,
@@ -41,7 +42,7 @@ contract Orders {
         address toCurrency
     );
     event OrderCanceled(
-        uint indexed orderId,
+        uint256 indexed orderId,
         address sender,
         address receiver,
         uint256 amount,
@@ -54,6 +55,7 @@ contract Orders {
         uint256 _amount,
         string memory _fromCurrency,
         address _toCurrency
+        string memory _locationId
     ) external {
         require(_amount > 0, "Amount must be greater than zero");
 
@@ -64,14 +66,15 @@ contract Orders {
             amount: _amount,
             fromCurrency: _fromCurrency,
             toCurrency: _toCurrency,
+            locationId: _locationId,
             status: OrderStatus.Created
         });
 
-        // A Chainlink oracle could exchange the cash amount for the USDC amount and store that 
+        // A Chainlink oracle could exchange the cash amount for the USDC amount and store that
         // # in the order. Later in completeOrder() a transfer to escrow can include that # + fee
 
         orders.push(newOrder);
-        uint orderId = orders.length + 1;
+        uint256 orderId = orders.length + 1;
         senderToOrders[msg.sender].push(orderId);
 
         emit OrderCreated(
@@ -103,7 +106,7 @@ contract Orders {
         emit OrderInProgress(_orderId, _courier);
     }
 
-    function completeOrder(uint _orderId) external {
+    function completeOrder(uint256 _orderId) external {
         require(_orderId < orders.length, "Order doesn't exist.");
         Order storage order = orders[_orderId - 1];
         require(
@@ -125,19 +128,80 @@ contract Orders {
             order.toCurrency
         );
 
-        // Transer funds from sender to either this or an escrow contract where it can hold the 
-        // courier's coins till they deposit their cash & send the intended amount to the 
+        // Transer funds from sender to either this or an escrow contract where it can hold the
+        // courier's coins till they deposit their cash & send the intended amount to the
         // receiver's wallet.
     }
 
-    function cancelOrder(uint _orderId) external returns (Order memory) {
+    function cancelOrder(uint256 _orderId) external returns (Order memory) {
         orders[_orderId - 1].status = OrderStatus.Canceled;
         // This will return a tuple that frontends can't handle
         return orders[_orderId - 1];
     }
 
-    function getOrder(uint _orderId) public view returns (Order memory) {
+    function getOrder(uint256 _orderId) public view returns (Order memory) {
         // This will return a tuple that frontends can't handle
         return orders[_orderId];
+    }
+
+    // backend off-chain function
+    // function isWithinRadius(bytes32 locationA, bytes32 locationB) internal pure returns (bool) {
+        // Perform your off-chain calculations to determine if locationA and locationB are within the desired radius.
+        // Return true if within radius, false otherwise.
+        // This is where you would use your off-chain location data.
+        // The actual implementation depends on your specific use case.
+        // Here, we're using a placeholder function for demonstration purposes.
+        // return true; // Modify this as needed
+    // }
+
+    function getCreatedOrders()
+        public
+        view
+        returns (Order[] memory)
+    {
+        Order[] memory regionalOrders;
+        uint256 orderCount = 0;
+
+        for (uint256 i = 0; i < orders.length; i++) {
+            // Perform off-chain calculations to determine if the order is within the desired radius.
+            if (isWithinRadius(orders[i].locationID, _courierLocationID)) {
+                orderCount++;
+            }
+        }
+
+        // Create a new array of the correct size
+        regionalOrders = new Order[](orderCount);
+        orderCount = 0;
+
+        for (uint256 i = 0; i < orders.length; i++) {
+            // Add orders within the radius to the regionalOrders array
+            if (isWithinRadius(orders[i].locationID, _courierLocationID)) {
+                regionalOrders[orderCount] = orders[i];
+                orderCount++;
+            }
+        }
+
+        return regionalOrders;
+
+        // USE THIS LOGIC BUT RETURN ORDERS AS ABOVE IN THIS FUNCTION
+        uint256[] memory createdOrderIds;
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < orders.length; i++) {
+            if (orders[i].status == OrderStatus.Created) {
+                createdOrderIds[count] = orders[i].orderId;
+                count++;
+            }
+        }
+
+        // Create a new dynamic array with the correct length
+        uint256[] memory result = new uint256[](count);
+
+        // Copy the order IDs to the new array
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = createdOrderIds[i];
+        }
+
+        return result;
     }
 }
