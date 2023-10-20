04fa424c3af6d15ad1874f7df2c2dc61a0bdb0cb
Nariman <nariman.mavliutov@gmail.com>
2023-10-20 22:43:08 +0700

    [mobile] wallet local

--- sendit.mobile/components/header/UserHeaderComponent.tsx
+++ sendit.mobile/components/header/UserHeaderComponent.tsx
@@ -1,41 +1,61 @@
-import { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import { ConnectWallet, useWallet, useDisconnect, useConnectionStatus } from '@thirdweb-dev/react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { ConnectWallet, useDisconnect, useConnectionStatus, useAddress, useCreateWalletInstance, useSetConnectionStatus, useSetConnectedWallet, localWallet } from '@thirdweb-dev/react-native';
+import { Ethereum, Mumbai } from '@thirdweb-dev/chains';
+
+import { truncateEthAddress } from '@/utils';
+
+const WALLET_PRIVATE_KEY = process.env.EXPO_PUBLIC_WALLET_PRIVATE_KEY;
 
 export function UserHeaderComponent() {
-    const [walletAddress, setWalletAddress] = useState<string>();
+    const createWalletInstance = useCreateWalletInstance();
+    const setConnectionStatus = useSetConnectionStatus();
+    const setConnectedWallet = useSetConnectedWallet();
 
-    const navigation = useNavigation();
+    const address = useAddress();
     const onDisconnect = useDisconnect();
     const connectionStatus = useConnectionStatus();
-    const wallet = useWallet();
 
     useEffect(() => {
-        async function getWalletAddress() {
-            setWalletAddress(await wallet.getAddress());
-        };
 
-        if (connectionStatus === 'connected') {
-            getWalletAddress();
+    }, []);
+
+    const onLocalWalletConnect = async () => {
+        const walletConfig = localWallet();
+        const wallet = createWalletInstance(walletConfig);
+
+        await wallet.import({
+            privateKey: WALLET_PRIVATE_KEY,
+            encryption: false
+        });
+
+        try {
+            setConnectionStatus('connecting');
+            await wallet.connect({ chainId: Mumbai.chainId });
+            setConnectedWallet(wallet);
+        } catch (e) {
+            setConnectionStatus('disconnected');
+            console.error('failed to connect', e);
         }
-    }, [connectionStatus]);
+    };
 
     return (
         <View style={styles.container}>
             <View>
                 {connectionStatus !== 'connected' && <>
-                    <ConnectWallet
-                        buttonTitle='Connect'
-                        detailsButton={() => {
-                            return <Button title="My Button" />;
-                        }}
-                    />
+                    {!WALLET_PRIVATE_KEY && <>
+                        <ConnectWallet buttonTitle='Connect' />
+                    </>}
+                    {WALLET_PRIVATE_KEY && <>
+                        <TouchableOpacity onPress={onLocalWalletConnect} style={styles.button}>
+                            <Text style={styles.buttonText}>Connect Local</Text>
+                        </TouchableOpacity>
+                    </>}
                 </>}
                 {connectionStatus === 'connected' && <>
-                    <Text>{walletAddress}</Text>
                     <TouchableOpacity onPress={() => onDisconnect()} style={styles.button}>
                         <Text style={styles.buttonText}>Disconnect</Text>
+                        <Text style={{ ...styles.buttonText, fontSize: 12, fontWeight: 'normal' }}>{truncateEthAddress(address?.toString())}</Text>
                     </TouchableOpacity>
                 </>}
             </View>
@@ -48,6 +68,7 @@ const styles = StyleSheet.create({
 
     },
     button: {
+        alignItems: 'center',
         marginHorizontal: 10,
         paddingHorizontal: 10,
         paddingVertical: 6,
